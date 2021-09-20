6c22f6027bf8bd676cb4a33cf5f5622b6c4ecf64
emi <emiliorizzo@gmail.com>
2021-09-20 14:13:53 -0300

    Set Z-OFFSET, 3dTouch, custom support

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1196,7 +1196,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 24, -37, -1.5 }
+#define NOZZLE_TO_PROBE_OFFSET { 24, -37, -1.414 }
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
 #define PROBING_MARGIN 10
